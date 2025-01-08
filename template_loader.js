d13c5b65562f24340ee21604a88b9f5a066addb6
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-01-08 15:57:36 -0600

    removed loadHead() function

--- template_loader.js
+++ template_loader.js
@@ -1,11 +1,19 @@
-function loadHead(tn){
-    fetch('head.html')
-      .then(response => response.text())
-      .then(data => {
-        // Create a regular expression to find `{{key}}`
-        const regex = new RegExp(`{{tabName}}`, 'g');
-        data = data.replace(regex, tn);
-      document.head.innerHTML = data;
-      });
+function loadHeader(currentPage){
+    // const currentPage = window.location.href;
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = data;
+            const links = tempDiv.querySelectorAll('a');
+            for (let i=0; i<links.length; i++){
+                // alert(links[i]);
+                if (links[i].getAttribute('href') === currentPage){
+                    links[i].classList.add('active');
+                }
+            }
+            document.getElementById('header').innerHTML = tempDiv.innerHTML;
+            document.getElementById('header').style.display = 'block';
+        })
 }
 
