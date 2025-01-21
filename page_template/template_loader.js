8aa1c708133f58ff3f88b98bb71d8107be4af800
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-01-20 22:54:19 -0600

    huge change to template_loader.js

--- page_template/template_loader.js
+++ page_template/template_loader.js
@@ -14,6 +14,7 @@ function loadHeader(currentPage){
             }
             document.getElementById('header').innerHTML = tempDiv.innerHTML;
             document.getElementById('header').style.display = 'block';
+            initMobileNav();
         })
 }
 
@@ -27,3 +28,36 @@ function loadFooter(){
             document.getElementById('footer').innerHTML = tempDiv.innerHTML;
         })
 }
+
+function initMobileNav() {
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  
+    function mobileNavToggle() {
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+      mobileNavToggleBtn.classList.toggle('bi-list');
+      mobileNavToggleBtn.classList.toggle('bi-x');
+    }
+  
+    if (mobileNavToggleBtn) {
+      mobileNavToggleBtn.addEventListener('click', mobileNavToggle);
+    }
+  
+    // Hide mobile nav on same-page/hash links
+    document.querySelectorAll('#navmenu a').forEach(navmenu => {
+      navmenu.addEventListener('click', () => {
+        if (document.querySelector('.mobile-nav-active')) {
+          mobileNavToggle();
+        }
+      });
+    });
+
+    document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+        navmenu.addEventListener('click', function(e) {
+          e.preventDefault();
+          this.parentNode.classList.toggle('active');
+          this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+          e.stopImmediatePropagation();
+        });
+      });
+  }
+  
